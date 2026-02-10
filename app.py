5ac54eec337a97237844b3ebc469bdb3ce31b571
Williambee22 <weblong2002@gmail.com>
2026-02-09 18:08:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1167,13 +1167,13 @@ def create_app() -> Flask:
 
 from flask import Flask
 
-app = Flask(__name__)
-
 app = create_app()
 
 if __name__ == "__main__":
     # Local dev only
 
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
